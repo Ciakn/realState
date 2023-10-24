e5e9f64420c43af2e06410495a4f713ee8754eb0
ciavash <svsh.kian@gmail.com>
2023-10-24 09:30:06 -0700

    singlehouse buttons

--- src/Components/SingleHouse.jsx
+++ src/Components/SingleHouse.jsx
@@ -6,11 +6,11 @@ function SingleHouse() {
   const { id } = useParams();
   const { data: house, isLoading } = useFetch("", id);
   if (isLoading) return <p>Loading...</p>
-console.log(house);
+  console.log(house);
 
   return (
     <div>
-   
+
       <div className='single-house_container'>
         <div className='single-house_img'>
           <img src={house.img} alt="" />
@@ -18,9 +18,13 @@ console.log(house);
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ratione.
 
           </p>
+          <div className='single-house_buttons '>
+            <button className='btn bg-success text-light ' >Contact info</button>
+            <button className='btn bg-danger text-light' >BookMark</button>
+          </div>
         </div>
       </div>
-  </div>
+    </div>
   )
 }
 
